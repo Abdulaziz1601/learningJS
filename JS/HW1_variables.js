51b69d7d07d7d36f24299afdc6a9e76abcf6b737
Abdulaziz <abdulazizfaactory@gmail.com>
2021-01-30 13:07:47 +0500

    HW2 done

--- JS/HW1_variables.js
+++ JS/HW1_variables.js
@@ -29,6 +29,7 @@
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -36,11 +37,14 @@ const personalMovieDB = {
     genres:[],
     private: false
 };
+
 const a = prompt('Один из последних просмотренных фильмов?',''),
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?',''),
-d = prompt('На сколько оцените его?', '');
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?', '');
+
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+
 console.log(personalMovieDB);
     
