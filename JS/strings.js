5e2cdf2c0543e5fbf13d3d415acc3223c70a94ef
Abdulaziz <abdulazizfaactory@gmail.com>
2021-01-31 17:53:41 +0500

    String and Number functions

---
+++ JS/strings.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// let str =  "something";
+// const arr = [1, 2, 3];
+// console.log(str.length); // lenght of string is 9
+// console.log(arr.length); // lenght of array is 3
+// console.log(str[2]); // m
+// console.log(str[1] = 'm'); // Cant be assigned 
+
+// console.log(str.toUpperCase()); //SOMETHING
+// console.log(str); //something
+// console.log(str.toUpperCase()); //SOMETHING
+// console.log(str.toLowerCase()); //something
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit")); // 5
+// // at 5th index "fruit" is started 
+// const logg = "Hello World";
+
+// console.log(logg.slice(6,11));// slice() arg1 -> starting from what index cut the string, arg2 -> till what index cut
+// arg2 -> dont include chosen element
+// const fullName = "Abdullaev Abdulaziz Abdunabievich";
+// const Lname = fullName.slice(0,9);
+// const Uname = fullName.slice(10,19);
+// const Sname = fullName.slice(20); 
+// console.log(`${Lname} ${Uname} ${Sname} `);
+// console.log(fullName.slice(-34,-20));
+
+// console.log(fullName.substring(0,9));// Abdullaev
+// // Same as slice but arguments cant be negative 
+
+// console.log(fullName.substr(0,9));// Abdullaev
+// same as above but arg2 -> inputs how many symbols to cut
+
+// const num = 12.2;
+// console.log(Math.round(num)); //12
+ // rounds to nearest number
+
+// const test = "12.2px";
+// console.log(parseInt(test)); // 12
+// String to num
+// console.log(parseFloat(test)); // 12.2
+// String to number with float
+
+
+
+
+
+
+
+
+
+
+
+
+ 
