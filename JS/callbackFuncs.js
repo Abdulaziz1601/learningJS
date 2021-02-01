0c487e082e6f66bb56c500b9baa85f705ce57ba8
Abdulaziz <abdulazizfaactory@gmail.com>
2021-02-01 17:28:22 +0500

    CallBack functions

---
+++ JS/callbackFuncs.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// function first() {
+//     // do something
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+// // Functions do not go after another 
+// //  2  1 
+
+// function learnJS(lang, callback) {
+//     console.log(`I am learning: ${lang}`);
+//     callback();
+// }
+// learnJS('JavaScript', function() {
+//     console.log('I have aced this lesson');
+// });
+// // Callback funcs
+
+// function learnJS(lang, callback) {
+//     console.log(`I am learning: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('I have aced this lesson');
+// }
+
+// learnJS('JavaScript', done);
