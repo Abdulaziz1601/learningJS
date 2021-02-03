a78caa0d7b72bd462a739c7980237b9119387584
Abdullaev Abdulaziz <abdulazizfaactory@gmail.com>
2021-02-03 17:31:06 +0500

    How to pass by value and by reference, and spread operator

--- JS/arrays.js
+++ JS/arrays.js
@@ -1,6 +1,27 @@
 "use strict";
 
-const arr = [1, 2, 3, 4, 5, 6];
+const arr = [100, 29, 34, 804, 15, 6];
+arr.sort(compareNum);// uses quick sort algorithm
+console.log(arr);// sort(), sorts only strings
+
+function compareNum(a, b) {
+    return a-b;
+}
+
+// arr[99] = 5;
+// console.log(arr);
+
+// arr.forEach(function(element, i, arr) {
+//     console.log((`${i}: ${element} in array ${arr}`));
+// // });
+
+// const str = prompt("", "");// We input some products like: p1, p2, p3... 
+// const products = str.split(", ");// split deletes commas and puts products in one array
+// // console.log(products); //[" p1", "p2", "p3"]
+// products.sort();
+// console.log(products.join("; ")); // aa; ggg; hhh; ccc
+
+
 
 // arr.pop(); // Deletes only last elem of arr
 // console.log(arr); //[ 1, 2, 3, 4, 5 ]
@@ -14,6 +35,6 @@ const arr = [1, 2, 3, 4, 5, 6];
 
 //for of -loop
 
-for (let val of arr) {
-    console.log(val);// = arr[i]
-}
+// for (let val of arr) {
+//     console.log(val);// = arr[i]
+// }
