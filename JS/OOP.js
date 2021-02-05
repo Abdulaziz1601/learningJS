57b72120a8bb8767a24e4d9244d0e2473cdfddf6
Abdullaev Abdulaziz <abdulazizfaactory@gmail.com>
2021-02-05 12:12:40 +0500

    Prototypes and OOP in JS

---
+++ JS/OOP.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let str = "some";
+let strObj = new String(str);
+
+// console.log(typeof(str));// string
+// console.log(typeof(strObj)); // object
+
+// console.dir([1, 2, 3]);
+
+const soldier =  {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hellooou");
+    }
+};
+
+const john = Object.create(soldier);
+
+john.sayHello();
+
+// const john = {
+    // health: 100
+// };
+
+
+// john.__proto__ = soldier; // not used |
+
+// Object.setPrototypeOf(john, soldier); // USED
+
+
+// console.log(john.armor);
+
+// john.sayHello();
