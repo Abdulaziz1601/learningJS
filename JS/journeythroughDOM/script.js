453cecc2cdfe02fcad5994fa644a671cf4ddd55f
Abdullaev Abdulaziz <abdulazizfaactory@gmail.com>
2021-02-10 19:35:12 +0500

    Data Attributes

---
+++ JS/journeythroughDOM/script.js
@@ -0,0 +1,8 @@
+// console.log(document.head); //head only
+// console.log(document.documentElement);// HTML tag
+// console.log(document.body.childNodes); // Access all Child nodes of given elem
+// console.log(document.body.firstChild); //access first child of an elem
+// console.log(document.body.lastChild); //access last child of an elem
+ 
+console.log(document.querySelector("#current").parentNode); //parent of btn 
+console.log(document.querySelector("#current").parentNode.parentNode); //grandParent of btn 
