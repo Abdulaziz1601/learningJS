28aed0b6828c39c89af0923fdc7b4bf5c09ceebb
Abdullaev Abdulaziz <abdulazizfaactory@gmail.com>
2021-02-10 20:00:25 +0500

    DOM traversing

--- JS/journeythroughDOM/script.js
+++ JS/journeythroughDOM/script.js
@@ -1,8 +1,26 @@
 // console.log(document.head); //head only
 // console.log(document.documentElement);// HTML tag
 // console.log(document.body.childNodes); // Access all Child nodes of given elem
-// console.log(document.body.firstChild); //access first child of an elem
-// console.log(document.body.lastChild); //access last child of an elem
+// console.log(document.body.firstChild); //access first child Node
+// console.log(document.body.firstElementChild); //access first child element
+
+// console.log(document.body.lastChild); //access last child Node
+// console.log(document.body.lastElementChild); //access last child  element
  
-console.log(document.querySelector("#current").parentNode); //parent of btn 
-console.log(document.querySelector("#current").parentNode.parentNode); //grandParent of btn 
+// console.log(document.querySelector("#current").parentNode); //parent of btn 
+// console.log(document.querySelector("#current").parentElement);
+
+// console.log(document.querySelector("#current").parentNode.parentNode); //grandParent of btn 
+
+// console.log(document.querySelector('[data-current="3"]').nextSibling); // Next line character
+// console.log(document.querySelector('[data-current="3"]').previousSibling); // Next line character
+
+// We do not have property to access all child elements, so we can make it up, by loop
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == "#text" ) {
+//         continue;
+//     } else {
+//         console.log(node);
+//     }
+// }
+
