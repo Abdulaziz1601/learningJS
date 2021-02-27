fdac809768c721b7a839979543e896946c563087
abdulaziz <abdulazizfaactory@gmail.com>
2021-02-27 15:15:00 +0500

    Constructor Functions and Classes

--- Module_2/constructorFunctions/main.js
+++ Module_2/constructorFunctions/main.js
@@ -5,5 +5,36 @@
 
 function User(name, id) {
     this.name = name;
-    
+    this.id = id;
+    this.human = true;
+    this.hello = () => console.log(`Hello ${this.name}`); //So we can allso use functions
+} //By this we can assign this function as a CONSTRUCTOR
+
+User.prototype.exit = function() {
+    console.log(`Bye ${this.name}`);
+};
+
+const abdulaziz = new User('Abdulaziz', 19); //It creates new object
+const ivan = new User('Ivan', 27);
+
+abdulaziz.hello();
+
+abdulaziz.exit();
+// This is used in ES5
+// -------------------------------------------------------------------------------------------------------------------
+// ES6 But classes are easier to use and they are syntactic sugar 
+// In computer science, syntactic sugar is syntax within
+// a programming language that is designed to make things easier to read or to express.
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;    
+    }
+    hello() {
+        console.log(`Hello ${this.name}`);
+    }
+    exit() {
+        console.log(`Bye ${this.name}`);
+    }
 }
