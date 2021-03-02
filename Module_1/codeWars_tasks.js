bad3ce843a78f572d9990dd6bca4fd196439802d
abdulaziz <abdulazizfaactory@gmail.com>
2021-03-02 13:55:30 +0500

    Code Wars kata is done

--- Module_1/codeWars_tasks.js
+++ Module_1/codeWars_tasks.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but 
+// exactly 4 digits or exactly 6 digits.
 
 // If the function is passed a valid PIN string, return true, else return false.
 
@@ -9,18 +10,18 @@
 // "12345"  -->  false
 // "a234"   -->  false
 
-function validatePIN (pin) {
-// var arr = pin.toString(10).replace(/\D/g, '0').split('').map(Number);
-let arr = Array.from(String(pin), Number);
- if( arr.length == 6 || arr.length == 4) {
-    for (const element of arr) {
-        if(typeof(element) == "number" )
-            return true;
-    }
-}
- return false;
-}
-console.log(validatePIN(23425));
+// function validatePIN (pin) {
+// // var arr = pin.toString(10).replace(/\D/g, '0').split('').map(Number);
+// let arr = Array.from(String(pin), Number);
+//  if( arr.length == 6 || arr.length == 4) {
+//     for (const element of arr) {
+//         if(typeof(element) == "number" )
+//             return true;
+//     }
+// }
+//  return false;
+// }
+// console.log(validatePIN(23425));
 
 // function addBinary(a,b) {
 //     let sum = a+b;
@@ -32,3 +33,22 @@ console.log(validatePIN(23425));
 //     return binary;
 //   }
 // console.log(addBinary(1,3)); 
+
+function solution(number){
+    let sum = 0;
+    for(let i=1;i<number; i++) {
+
+      if(i < 0) {
+        return 0;
+      } else if(i % 3 == 0 && i % 5 == 0) {
+        sum += i;
+        continue;
+      } else if(i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+}
+console.log(solution(30));
+
+// all true perfect solution()
