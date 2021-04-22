3c203fad0663a4191f98b068f5078666aa583bac
Abdulaziz <abdulazizfaactory@gmail.com>
2021-04-22 23:16:40 +0500

    LocalStorage is learnt, starting to practice

---
+++ Module_2/LocalStorage/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// window.localStorage // storage can hold 5mb of data
+
+// will write a key - number, with value - 5; number: 5 
+// localStorage.setItem('number', 5);
+
+
+// localStorage.getItem('number'); // Gets the value of number key
+
+// console.log(localStorage.getItem('number')); //   number: 5  
+
+// localStorage.removeItem('number'); // Deletes the value of number key
+
+// console.log(localStorage.getItem('number')); //   null
+
+// localStorage.setItem('number', 5000);
+
+// localStorage.clear(); // cleares all keys and values 
