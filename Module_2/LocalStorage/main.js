e80535d6dee08ec557ee41928e439383c388d46d
Abdulaziz <abdulazizfaactory@gmail.com>
2021-04-23 00:24:38 +0500

    LocalStorage is  practiced

--- Module_2/LocalStorage/main.js
+++ Module_2/LocalStorage/main.js
@@ -17,3 +17,38 @@
 // localStorage.setItem('number', 5000);
 
 // localStorage.clear(); // cleares all keys and values 
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = '#e56';    
+}
+checkbox.addEventListener('change', () => { // checkbox's status is changing so we take change event
+    localStorage.setItem('isChecked', true); //If checkb is checked then it will be true
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = '#e56';
+    }
+});
+
+const person = { //We wanna save this obj in localStorage
+    name: 'Alex',
+    age: 19
+};
+
+const serializedPerson = JSON.stringify(person); //made obj -> JSON
+localStorage.setItem('alex', serializedPerson); //made JSON -> obj
+
+console.log(JSON.parse(localStorage.getItem('alex')));
