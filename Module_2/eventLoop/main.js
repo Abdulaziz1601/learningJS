81a545af2a8ef195881a04140d0e5b22f2077caf
Abdulaziz <abdulazizfaactory@gmail.com>
2021-05-21 18:49:49 +0500

    learning event loop

---
+++ Module_2/eventLoop/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// let increment = 0;
+
+// function count() {
+//     for(let i = 0; i < 1e9; i++){increment++;}
+//     alert('done');
+// }
+
+// count();
+
+setTimeout(() => {
+    console.log(1);
+}, 0);
+
+console.log(2);
+
