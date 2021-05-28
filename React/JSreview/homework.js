0c97ec558e3e79422166bd646db4b8903245a061
Abdulaziz <abdulazizfaactory@gmail.com>
2021-05-28 18:54:38 +0500

    Woking on HW

--- React/JSreview/homework.js
+++ React/JSreview/homework.js
@@ -1,40 +1,34 @@
-var employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
-var employersNames = [];
-for (var i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].length != '') {
-		employersNames.push(employers[i]);
-	}
-}
-for (var i = 0; i < employersNames.length; i++) {
-	employersNames[i] = employersNames[i].toLowerCase().trim();
-}
+let employersNames = [];
 
-var sponsors = {
+employersNames = [...employers.filter((employer) => employer.length > 0 && employer.length != 0)];
+employersNames = employersNames.map((employerName) => employerName.toLowerCase().replace(/\s/g, ""));
+
+const sponsors = {
     cash: [40000, 5000, 30400, 12000],
     eu: ['SRL', 'PLO', 'J&K'],
     rus: ['RusAuto', 'SBO']
 };
 
-function calcCash(own) {
-    own = own || 0;
-    var everyCash = Array.prototype.slice.call(arguments);
-    var total = own;
-    for (var i = 0; i < everyCash[1].length; i++) {
-        total += +everyCash[1][i];
-    }
+const {cash: sponsorsCash, eu: euSponsors, rus: rusSponsors } = sponsors; //object destructuring used
+
+function calcCash(own=0) {
+    // let everyCash = Array.prototype.slice.call(arguments); // Turns to an array
+    let everyCash = [...arguments]; // spread operator also turns to an array
+    // finding sum of sponsor cash array([40000, 5000, 30400, 12000]) and adding it to own cash
+    const total = everyCash[1].reduce((current, sum) => sum+current, own)  
+    
     return total;
 }
 
-var money = calcCash(null, sponsors.cash);
+const money = calcCash(null, sponsorsCash);
 
-function makeBusiness(owner, director, cash, emp) {
-    director = director || 'Victor';
-    var sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
-    console.log('We have a business. Owner: ' + owner + ', director: ' + director + '. Our budget: ' + cash + '. And our employers: ' +
-    emp);
-    console.log('And we have a sponsors: ');
+function makeBusiness(owner, director='Victor', cash, emp) {
+    // var sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
+    const sumSponsors = [...euSponsors, ...rusSponsors, "unexpected sponsor"];
+    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}\nAnd we have a sponsors: `);
     console.log.apply(null, sumSponsors);
-    console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
+    console.log(`Note. Be careful with ${euSponsors[0]}. It's a huge risk.`);
 }
 makeBusiness.apply(null, ['Sam', null, money, employersNames]);
