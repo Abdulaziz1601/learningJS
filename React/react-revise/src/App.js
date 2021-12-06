ab43e228bec5de0b8ddb0fe9e108a099be0ff34a
Abdulaziz <abdulazizfaactory@gmail.com>
2021-12-06 16:31:24 +0500

    revised state and setState

--- React/react-revise/src/App.js
+++ React/react-revise/src/App.js
@@ -1,19 +1,42 @@
+import {Component} from 'react';
+
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-	return (
-		<div>
-			<h1>My name is {name()}, surname - {surname}</h1>
-			<a href={link}>My personal page</a>
-		</div>
-	)
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++'
+		};
+
+	}
+
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({
+			years: state.years + 1
+		}));
+	}
+
+
+	render() {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>	
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>My personal page</a>
+			</div>
+		);
+	}
 }
 
 function App() {
   return (
 	<div className="App">
-		<WhoAmI name={() => {return 'John'}} surname="Smith" link="google.com"/>
-		<WhoAmI name={() => {return 'John'}} surname="Smith" link="google.com"/>
+		<WhoAmI name='John' surname="Smith" link="google.com"/>
+		<WhoAmI name='Ali' surname="Smith" link="google.com"/>
 	</div>
   );
 }
