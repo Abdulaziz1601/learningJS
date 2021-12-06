df9c49d8f92cd78aa19f23f305d68783882612c1
Abdulaziz <abdulazizfaactory@gmail.com>
2021-12-06 14:00:46 +0500

    revising react

---
+++ React/react-revise/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function WhoAmI ({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name()}, surname - {surname}</h1>
+			<a href={link}>My personal page</a>
+		</div>
+	)
+}
+
+function App() {
+  return (
+	<div className="App">
+		<WhoAmI name={() => {return 'John'}} surname="Smith" link="google.com"/>
+		<WhoAmI name={() => {return 'John'}} surname="Smith" link="google.com"/>
+	</div>
+  );
+}
+
+export default App;
