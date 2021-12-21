e0d5eb0304336cee9c59500f03fbf44c8a52d051
Abdulaziz <abdulazizfaactory@gmail.com>
2021-12-21 10:45:00 +0500

    Styled components - styles

--- React/react-revise/src/App.js
+++ React/react-revise/src/App.js
@@ -1,6 +1,55 @@
-import {Component, } from 'react';
+import {
+	Component
+} from 'react';
+import styled from 'styled-components';
 
 import './App.css';
+
+const EmpItem = styled.div `
+	padding: 20px;
+	margin-bottom: 15px;
+	border-radius: 5px;
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+	transition: all .2s;
+	&:hover {
+		box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
+	}
+	a {
+		display: block;
+		margin: 10px 0 10px 0;
+		color: ${props => props.active ? 'orange' : 'black'};
+	}
+	input {
+		display: block;
+		margin-top: 10px;
+
+	}
+`;
+
+const Header = styled.h2 `
+	font-size: 2em;
+	text-align: center;
+	color: black
+`;
+
+export const Btn = styled.button `
+	display: block;
+	margin: 0 auto;
+	margin-top: 20px;
+	padding: 15px;
+	border: none;
+	background-color: skyblue;
+	color: #fff;
+	border-radius: 5px;
+	transition: all .2s;
+	&:hover {
+		background-color: black;
+		transform: translateY(-3px)
+	}
+	&:active {
+		transform: translateY(0px)
+	}
+`;
 
 class WhoAmI extends Component {
 	constructor(props) {
@@ -20,40 +69,57 @@ class WhoAmI extends Component {
 	}
 	commitInputChanges(e, color) {
 		console.log(color);
-		this.setState({position: e.target.value});
+		this.setState({
+			position: e.target.value
+		});
 	}
 
 
 
 	render() {
-		const {name, surname, link} = this.props;
-		const {position, text, years} = this.state;
+			const {
+				name,
+				surname,
+				link
+			} = this.props;
+			const {
+				position,
+				text,
+				years
+			} = this.state;
 
-		console.log(this);
+			console.log(this);
 
-		return (
-			<>
-				<button onClick={this.nextYear}>{text}</button>	
-				<h1>My name is {name}, surname - {surname}, 
-					age - {years}, 
-					<strong>position - {position}</strong></h1>
-				<a href={link}>My personal page</a>
-				<form> 
-					<span>Vvedite doljnost</span>
-					<input type="text" onChange={(e) => this.commitInputChanges(e, 'SOMEcolor')}></input>
-				</form>
-			</>
+		return ( 
+			<EmpItem active='false'>
+				<Header >
+					My name is {name}, surname - {surname}, <br/> age-{years}, <strong > position - {position} </strong> 
+				</Header>
+				<Btn onClick = {this.nextYear}>
+					{text}
+				</Btn>
+				<a href={link}> My personal page </a>
+				<form >
+					<span > Vvedite doljnost </span> 
+					<input type="text" onChange = {(e) => this.commitInputChanges(e, 'SOMEcolor')} /> 
+				</form >
+			</EmpItem>
 		);
 	}
-}
+	}
+
+const Wrapper = styled.div `
+	width: 600px;
+	margin: 80px auto 0 auto
+`;
 
 function App() {
-  return (
-	<div className="App">
-		<WhoAmI name='John' surname="Smith" link="google.com"/>
-		<WhoAmI name='Ali' surname="Smith" link="google.com"/>
-	</div>
-  );
+	return ( 
+		<Wrapper >
+			<WhoAmI name='John' surname="Smith" link="google.com"/>
+			<WhoAmI name='Ali' surname="Smith" link="google.com"/>
+		</Wrapper>
+	);
 }
 
 export default App;
--- React/react-revise/src/index.js
+++ React/react-revise/src/index.js
@@ -1,11 +1,20 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
 import './index.css';
 import App from './App';
+import {Btn} from './App';
 
+// Here we can use inheritance, so we can reuse our simple btn and make Big btn out of it
+const BigBtn = styled(Btn)`
+  width:245px;
+  text-align: center;
+`;
 ReactDOM.render(
     <StrictMode>
-      <App />
+      <App />,
+      <BigBtn as="a">Send Data</BigBtn>
     </StrictMode>,
   document.getElementById('root')
 );
