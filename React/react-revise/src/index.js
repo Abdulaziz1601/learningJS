61496f716fd6678f29658acb089fe42e76fe6295
Abdulaziz <abdulazizfaactory@gmail.com>
2021-12-21 12:01:12 +0500

    using bootstrap and etc.

--- React/react-revise/src/index.js
+++ React/react-revise/src/index.js
@@ -2,9 +2,11 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import {Btn} from './App';
+import BootstrapTest from './BootstrapTest';
 
 // Here we can use inheritance, so we can reuse our simple btn and make Big btn out of it
 const BigBtn = styled(Btn)`
@@ -14,7 +16,8 @@ const BigBtn = styled(Btn)`
 ReactDOM.render(
     <StrictMode>
       <App />,
-      <BigBtn as="a">Send Data</BigBtn>
+      <BigBtn as="a">Send Data</BigBtn>,
+      <BootstrapTest/>
     </StrictMode>,
   document.getElementById('root')
 );
