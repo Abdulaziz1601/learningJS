8df369b7a03566148560eef8bdb38db740d704de
Abdulaziz <abdulazizfaactory@gmail.com>
2022-01-18 18:30:29 +0500

    using class-component based state with useState hook

--- React/slider_useState_hook/src/App.js
+++ React/slider_useState_hook/src/App.js
@@ -49,22 +49,42 @@ import './App.css';
 
 const Slider = (props) => {
     // Destructurin an array   setting slide to zero
-    const [slide, setSlide] = useState(0); // returns array of two elems: 1-st elem -- state, 2nd elem -- function that will change that state
-    const [autoplay, setAutoplay] = useState(false);
+    // const [slide, setSlide] = useState(0); // returns array of two elems: 1-st elem -- state, 2nd elem -- function that will change that state
+    // const [autoplay, setAutoplay] = useState(false);
+    // Creating state like in Class-Based Components
+    const [state, setState] = useState({ // careful, you have to change smth according to immutability
+        slide: 0, // also, use state as an object
+        autoplay: false
+    });
+
     function changeSlide(i) {
-        setSlide(slide => slide+i)
-        setSlide(slide => slide+i)
+        setState(state => ({
+            ...state,
+            slide: state.slide + i
+        }));
+
     }
     function toggleAutoplay() {
-        setAutoplay(autoplay => !autoplay);
+        setState(state => ({
+            ...state,
+            autoplay: !state.autoplay
+        }));
     }
+    
+    // function changeSlide(i) {
+    //     setSlide(slide => slide+i)
+    //     setSlide(slide => slide+i)
+    // }
+    // function toggleAutoplay() {
+    //     setAutoplay(autoplay => !autoplay);
+    // }
     
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/> </div>
-                <div className="text-center mt-5">{autoplay ? 'AutopPlay' : null} <br/> </div>
+                <div className="text-center mt-5">Active slide {state.slide} <br/> </div>
+                <div className="text-center mt-5">{state.autoplay ? 'AutopPlay' : null} <br/> </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
