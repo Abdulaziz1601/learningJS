3796a9a2d0f030449ca98309932ba36ad164e52a
Abdulaziz <abdulazizfaactory@gmail.com>
2022-02-13 21:32:07 +0500

    learning Higher Order Components - HOC

---
+++ React/learning_hoc/src/App.js
@@ -0,0 +1,126 @@
+import {useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+// const f = (a) => {
+//     return (b) => {
+//         console.log(a + b);
+//     };
+// };
+
+// f(1)(2);
+
+// const f = (a) => {
+//     return class extends Component {
+//         render() {
+//             return <h1>Hello World</h1> ;
+//         }
+//     }
+// };
+
+// HOC for combining sliders
+// We could've passed getDataFromFirstFetch  getDataFromSecondFetch as props
+// But our slider's design is different than each other
+
+const withSlider = (BaseComponent, getData) => {
+    return (props) => {
+        const [slide, setSlide] = useState(0);
+        const [autoplay, setAutoplay] = useState(false)
+    
+        useEffect(() => {
+            setSlide(getData());
+        }, [])
+    
+        function changeSlide(i) {
+            setSlide(slide => slide + i);
+        }
+
+        return <BaseComponent
+                {...props}
+                slide={slide}
+                autoplay={autoplay}
+                setSlide={setSlide}
+                setAutoPlay={setAutoplay}/>;
+
+    }
+}
+
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
+
+const SliderFirst = (props) => {
+
+    // Deleting UNUSED functions and data 'cause we already defined them at withSlider component
+    // const [slide, setSlide] = useState(0);
+
+    // useEffect(() => {
+    //     setSlide(getDataFromFirstFetch());
+    // }, [])
+
+    // function changeSlide(i) {
+    //     setSlide(slide => slide + i);
+    // }
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide}</div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+const SliderSecond = (props) => {
+
+    // deleting UNUSED data
+    // const [slide, setSlide] = useState(0);
+    // const [autoplay, setAutoplay] = useState(false)
+
+    // useEffect(() => {
+    //     setSlide(getDataFromSecondFetch());
+    // }, [])
+
+    // function changeSlide(i) {
+    //     setSlide(slide => slide + i);
+    // }
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{aprops.utoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <SliderFirst/>
+            <SliderSecond/>
+        </>
+    );
+}
+
+export default App;
