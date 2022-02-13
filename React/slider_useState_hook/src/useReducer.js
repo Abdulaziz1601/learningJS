d5481a2014dd2dbc5731df05ba36abf60dc56963
Abdulaziz <abdulazizfaactory@gmail.com>
2022-02-13 14:37:28 +0500

    learning useReducer hook

--- React/slider_useState_hook/src/useReducer.js
+++ React/slider_useState_hook/src/useReducer.js
@@ -2,10 +2,31 @@ import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-const Slider = () => {
+function reducer (state, action) {
+    switch (action.type) {
+        case "toggle":
+            return {autoplay: !state.autoplay};
+        case 'slow':
+            return {autoplay: 300};
+        case 'fast':
+            return {autoplay: 700};
+        case 'custom':
+            return {autoplay: action.payload};
+        default:
+            throw new Error();
+    }
+    
+}
+
+function init(initial) {
+    // common use in async funcition, lazy-loading of inittial value:
+    return {autoplay: initial};
+}
+
+const Slider = ({initial}) => {
     const [slide, setSlide] = useState(0);
     // const [autoplay, setAutoplay] = useState(false);
-    const [autoplay, dispatch] = useReducer();
+    const [autoplay, dispatch] = useReducer(reducer, initial, init);
 
 
     function changeSlide(i) {
@@ -16,7 +37,7 @@ const Slider = () => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
@@ -26,7 +47,16 @@ const Slider = () => {
                         onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => setAutoplay(!autoplay)}>toggle autoplay</button>
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
                 </div>
             </div>
         </Container>
@@ -35,7 +65,7 @@ const Slider = () => {
 
 function App() {
     return (
-        <Slider/>
+        <Slider initial={false}/>
     );
 }
 
