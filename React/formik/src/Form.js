963591e2ab3dbb68bbe9a20f5ba7373ee4013a9c
Abdulaziz <abdulazizfaactory@gmail.com>
2022-03-23 19:48:42 +0500

    form validation with yup

--- React/formik/src/Form.js
+++ React/formik/src/Form.js
@@ -1,22 +1,5 @@
-import { useFormik } from 'formik';
-
-const validate = (values) =>  {
-    const errors = {};
-
-    if (!values.name) {
-        errors.name = 'Obyazatelnoe pole!';
-    } else if(values.name.length < 2) {
-        errors.name = "Minimum 2 simvola dlya zapolneniya!";
-    }
-
-    if (!values.email) {
-        errors.email = 'Obyazatelnoe pole!';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Nepravilniy email address';
-    }
-
-    return errors;
-}
+import { FormikConsumer, useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Form = () => {
 
@@ -29,7 +12,14 @@ const Form = () => {
             text: '',
             terms: false,
         },
-        validate,
+        validationSchema: Yup.object({
+            name: Yup.string().min(2, 'Minimum 2 simvola').required('Obyazatelnoe pole!'),
+            email: Yup.string().email('Nepravilniy email address!').required('Obyazatelnoe pole!'),
+            amount: Yup.number().min(5, 'Ne menee 5').max(1000).required('Obyazatelnoe pole!'),
+            currency: Yup.string().required('Viberite valyutu'),
+            text: Yup.string().min(10, "Ne menee 10 simvolov!").required(),
+            terms: Yup.boolean().required('Neobxodimo soglasie!').oneOf([true], 'Neobxodimo soglasie!'),
+        }),
         onSubmit: values => {
             console.log(JSON.stringify(values, null, 2));
         }
@@ -49,7 +39,7 @@ const Form = () => {
                 onBlur={formik.handleBlur}
             />
             {/* When name is interacted touched.name will be TRUE */}
-            {formik.errors.name && formik.touched.name ? <div>{formik.errors.name}</div> : null}
+            {formik.errors.name && formik.touched.name ? <div className="error">{formik.errors.name}</div> : null}
             <label htmlFor="email">Ваша почта</label>
             <input
                 id="email"
@@ -59,7 +49,7 @@ const Form = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
             />
-            {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
+            {formik.errors.email && formik.touched.email ? <div className="error">{formik.errors.email}</div> : null}
             <label htmlFor="amount">Количество</label>
             <input
                 id="amount"
@@ -69,6 +59,7 @@ const Form = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
             />
+            {formik.errors.amount && formik.touched.amount ? <div className="error">{formik.errors.amount}</div> : null}
             <label htmlFor="currency">Валюта</label>
             <select
                 id="currency"
@@ -82,6 +73,7 @@ const Form = () => {
                     <option value="UAH">UAH</option>
                     <option value="RUB">RUB</option>
             </select>
+            {formik.errors.currency && formik.touched.currency ? <div className="error">{formik.errors.currency}</div> : null}
             <label htmlFor="text">Ваше сообщение</label>
             <textarea 
                 id="text"
@@ -90,6 +82,7 @@ const Form = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
             />
+            {formik.errors.text && formik.touched.text ? <div className="error">{formik.errors.text}</div> : null}
             <label className="checkbox">
                 <input
                     name="terms"
@@ -100,6 +93,7 @@ const Form = () => {
                     />
                 Соглашаетесь с политикой конфиденциальности?
             </label>
+            {formik.errors.terms && formik.touched.terms ? <div className="error">{formik.errors.terms}</div> : null}
             <button type="submit">Отправить</button>
         </form>
     )
