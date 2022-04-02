a06a3a3aeb73c3081d884980f49b04d1e9b47955
Abdulaziz <abdulazizfaactory@gmail.com>
2022-04-02 10:49:53 +0500

    how to implment checkbox with useField hook

--- React/formik/src/Form.js
+++ React/formik/src/Form.js
@@ -1,5 +1,36 @@
 import { Formik, Form, Field, ErrorMessage, useField } from 'formik';
 import * as Yup from 'yup';
+// Optimization when we have a lot of same inputs
+const MyTextInput = ({label, ...props}) => {
+
+    // Hook useField will attach our input/label to our Formik component with the help of Context to include such props like onSubmit, onChange, onBlur and etc.
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.name}>{ label }</label>
+            <input {...props} {...field} />
+            {meta.touched && meta.error ? (
+                <div className='error'>{meta.error}</div>
+            ) : null}
+        </>
+    )
+};
+
+const MyCheckBox = ({children, ...props}) => {
+    const [field, meta] = useField({...props, type: 'checkbox' });
+    return (
+        <>
+            <label className='checkbox'>
+                <input type="checkbox" {...props} {...field} />
+                {children}
+            </label>
+
+            {meta.touched && meta.error ? (
+                <div className='error'>{meta.error}</div>
+            ) : null}
+        </>
+    )
+};
 
 const CustomForm = () => {
 
@@ -39,20 +70,26 @@ const CustomForm = () => {
         >
             <Form className="form" >
                 <h2>Отправить пожертвование</h2>
-                <label htmlFor="name">Ваше имя</label>
+                {/* <label htmlFor="name">Ваше имя</label>
                 <Field
                     id="name"
                     name="name"
                     type="text"
                 />
-                <ErrorMessage className="error" name="name" component="div"/>                
-                <label htmlFor="email">Ваша почта</label>
-                <Field
+                <ErrorMessage className="error" name="name" component="div"/>*/}
+                {/* Instead of those three components we will write only one below */}
+                <MyTextInput 
+                    label={"Ваше имя"}
+                    id="name"
+                    name="name"
+                    type="text"
+                />
+                <MyTextInput 
+                    label="Ваша почта"
                     id="email"
                     name="email"
                     type="email"
-                />
-                <ErrorMessage className="error" name="email" component="div"/>                
+                />        
                 <label htmlFor="amount">Количество</label>
                 <Field
                     id="amount"
@@ -77,13 +114,17 @@ const CustomForm = () => {
                     name="text"
                     as="textarea"/>
                 <ErrorMessage className="error" name="text" component="div"/>                
-                <label className="checkbox">
+                {/* <label className="checkbox">
                     <Field
                         name="terms"
                         type="checkbox" />
                     Соглашаетесь с политикой конфиденциальности?
                 </label>
-                <ErrorMessage className="error" name="terms" component="div"/>                
+                <ErrorMessage className="error" name="terms" component="div"/>                 */}
+                <MyCheckBox 
+                    name="terms">
+                        Соглашаетесь с политикой конфиденциальности?
+                </MyCheckBox>
                 <button type="submit">Отправить</button>
             </Form>
         </Formik>
