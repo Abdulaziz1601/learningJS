149018b821db5e984ce874c4ac00fc49e32cfd66
Abdulaziz <abdulazizfaactory@gmail.com>
2022-04-25 00:17:13 +0500

    simple redux counter

---
+++ React/react-redux/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createStore} from 'redux';
+
+
+// ACTION INCREMENT
+const increment = () => {
+	return {
+		type: 'INCREMENT',
+	}
+}
+
+const decrement = () => {
+	return {
+		type: 'DECREMENT',
+	}
+};
+
+// REDUCER
+
+const counter = (state = 0, action) => {
+	switch(action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+	}
+}
+
+let store = createStore(counter);
+
+// Display it in the console 
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
