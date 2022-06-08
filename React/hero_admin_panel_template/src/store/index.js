0d24ef6405e26521f000de4c5de86e04bf923001
Abdulaziz <abdulazizfaactory@gmail.com>
2022-06-08 18:33:07 +0500

    redux toolkit, starter

--- React/hero_admin_panel_template/src/store/index.js
+++ React/hero_admin_panel_template/src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit'
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -13,13 +12,22 @@ const stringMiddleware = (store) => (next) => (action) => {
     return next(action);
 }
 
-const store = createStore(
-                    combineReducers({heroes, filters}),
-                    compose(
-                        applyMiddleware(ReduxThunk, stringMiddleware),
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    )
-                    );
+// const store = createStore(
+//                     combineReducers({heroes, filters}),
+//                     compose(
+//                         applyMiddleware(ReduxThunk, stringMiddleware),
+//                         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//                     )
+//                     );
+
+const store = configureStore({
+    // two reducers instead of combineReducers
+    reducer: {heroes, filters},
+    // adding middlewares, common middlewares as ReduxThunk and etc. are already included in toolkit
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    // checks if our app is in development we use devTools if not we do not use devTools
+    devTools: process.env.NODE_ENV !== 'production',
+})
 
 export default store;
 
