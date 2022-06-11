339e54982265e313f0dd34d2a325e4d3ec57d44e
Abdulaziz <abdulazizfaactory@gmail.com>
2022-06-11 15:55:16 +0500

    createAsyncThunk, fetchHeroes is added to heroesSlice

--- React/hero_admin_panel_template/src/components/heroesList/heroesSlice.js
+++ React/hero_admin_panel_template/src/components/heroesList/heroesSlice.js
@@ -1,10 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {useHttp} from '../../hooks/http.hook';
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
-}   
+};
+
+export const fetchHeroes = createAsyncThunk(
+    // name of slice, and type
+    'heroes/fetchHeroes',
+    // function that return Promise, async code
+    // parameters are arguments called with this fetchHeroes, for ex: can be id that fetches user with unique id
+    () => {
+        const {request} = useHttp();
+        return request("http://localhost:3001/heroes");
+    }
+
+); // It returs us 3 types of actions
+// pending
+// fullfilleed
+// rejected
 
 
 const heroesSlice = createSlice({
@@ -12,21 +27,24 @@ const heroesSlice = createSlice({
     initialState,
     reducers: {
         // actionCreators and Its Reducer
-        heroesFetching: state => {state.heroesLoadingStatus='loading'},
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: (state, action) => {
-                    state.heroesLoadingStatus = 'idle';
-                    state.heroes = action.payload;
-        },
         heroCreated: (state, action) => {
                     state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
                     state.heroes = state.heroes.filter(item => item.id !== action.payload)
         }
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus='loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload; // all returned data from promise will go into action.payload
+            })
+            .addCase(fetchHeroes.rejected, state => { state.heroesLoadingStatus = 'error'; })
+            .addDefaultCase(() => {})
+
     }
 });
 
