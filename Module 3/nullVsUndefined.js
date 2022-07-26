f1ad27fa9c418705de488d47dfd444e9e2f8f3b8
Abdulaziz <abdulazizfaactory@gmail.com>
2022-07-26 17:09:29 +0500

    null versus undefined

---
+++ Module 3/nullVsUndefined.js
@@ -0,0 +1,18 @@
+// null and undefined both mean that there is no value to access
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+// Only difference between Null and Undefined
+// Is the INTENT that programmers is gonna show us
+// using the keyword null or undefined
+
+// null specifically tells us that this variable has no value 
+
+// undefined says, It has not been defined yet
+
+let a;
+
+console.log(a); // undefined - tells us to actually defined it and use it
+
+// However defining smth with null, tells us that variable was defined but It has no value at all
